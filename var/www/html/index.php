ac25053568b8d0ba6cae71a28d70f43b44e0dd54
Maarti <bryan.martinet@gmail.com>
2016-10-12 23:44:01 +0200

    new web homepage, bigger for mobile

--- var/www/html/index.php
+++ var/www/html/index.php
@@ -1,33 +1,45 @@
 <!DOCTYPE html>
 <html>
 <head>
-<title>Vidéos</title>
+	<title>Dates</title>
+	<style>
+	body
+	{
+		color:white;
+		background-color: #152836;
+		font-size:20px;
+	}
+	ul{margin-left:10px;}
+	li{margin-bottom:20px; font-size:30px}
+	a{color:white}
+	</style>
 </head>
 <body>
-<?php
-$dir = '/var/www/html/cam';
-
+	<h1>Dates</h1>
+	<hr/>
+	<div>
+	<ul>
+	<?php
+	$dir = '/var/www/html/cam';
 	$dirs = scandir($dir, 1);	// 0 = croissant,  >0 = decroissant
 
-	//print_r($dirs);
-
 	foreach ($dirs as $num => $dir_name){
-		if(substr($dir_name,0,1)!='.'){
 		// $dir_name est de la forme "20160825"
-		//$dir_exp = explode("-", $dir_name);
-		$year = substr($dir_name,0,4);
-		$month = substr($dir_name,4,2);
-		$day = substr($dir_name,6,2);
-		//$dir_display = $dir_exp[2].'/'.$dir_exp[1].'/'.$dir_exp[0];
-		$dir_display = $day.'/'.$month.'/'.$year;
+		if(substr($dir_name,0,1)!='.'){		
+			$year = substr($dir_name,0,4);
+			$month = substr($dir_name,4,2);
+			$day = substr($dir_name,6,2);
+			$timestamp = strtotime($year.'-'.$month.'-'.$day);
+			$dayStr = date('D', $timestamp);
+			$dir_display = $dayStr.' '.$day.'/'.$month.'/'.$year;
 
-		echo '<li><a href=video.php?date='.$dir_name.'>'.$dir_display.'</a></li>';
+			echo '<li><a href=video.php?date='.$dir_name.'>'.$dir_display.'</a></li>';
 		}
 	}
-
 	unset($num);
-
 	unset($dir_name);
-?>
+	?>
+	</ul>
+	</div>
 </body>
 </html>
